0525c6ea9fe29b03781400cc963f48ff673c03a6
David Anaeme <davidcanaeme@gmail.com>
2024-08-16 17:28:04 -0600

    addIngredients

---
+++ src/components/BurgerStack.jsx
@@ -0,0 +1,14 @@
+// `src/components/BurgerStack.jsx`
+const BurgerStack = ({stack}) => {
+  return (
+    <ul>
+      {stack.map((ingredient, index) => (
+        <li key={index} style={{ backgroundColor: ingredient.color }}>
+          {ingredient.name}
+        </li>
+      ))}
+    </ul>
+  );
+  };
+  
+  export default BurgerStack;
